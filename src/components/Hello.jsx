35531136bc0b63ec88bba2116262bec26d4c31fa
Sonne <carlosh.mitma@gmail.com>
2020-01-06 00:46:53 -0500

    first commit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../assets/styles/styles.scss';
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Simple React</h1>
+                <p>React, Webpack, Sass, Babel, Eslint</p>
+            </div>
+        )
+    }
+}
