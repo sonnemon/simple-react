0a5cfa9ca01ceb4bb0ad456176538f83d1950ade
Sonne <carlosh.mitma@gmail.com>
2020-01-14 00:13:30 -0500

    add react-router-dom

---
+++ src/containers/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Home() {
+	return (
+		<div>
+			<h1>Simple React</h1>
+			<p>React, Webpack, Sass, Babel, Eslint</p>
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Hello from './components/Hello'
-ReactDOM.render(<Hello />, document.getElementById("app"))
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './router/App';
+import './assets/styles/styles.scss';
+ReactDOM.render(<App />, document.getElementById('app'));
